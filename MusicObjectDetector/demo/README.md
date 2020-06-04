1805e177c36579186253c5ffdfc3bd48cc124a21
Yvan Satyawan <y_satyawan@hotmail.com>
2020-06-04 11:14:48 +0200

    Updated Dockerfile to now be fully functional for inference

--- MusicObjectDetector/demo/README.md
+++ MusicObjectDetector/demo/README.md
@@ -1,19 +1,46 @@
 # Running this demo
 
-Download the trained model from 
-- [2018-07-30_faster-rcnn_inception-resnet-v2_full-page_muscima.pb](https://github.com/apacha/MusicObjectDetector-TF/releases/download/full-page-detection-v2/2018-07-30_faster-rcnn_inception-resnet-v2_full-page_muscima.pb)
+1. Build the docker image and tag it. In this case we will call it saty/muscima.
+   ```bash
+   docker build MusicObjectDetector/demo/ -t saty/muscima
+   ```
+2. All hand writen music sheets should then be put in a single directory. In this example the directory will be `/sheet_music`
+3. Run the docker image with a volume mounted to the directory mapped to `/workspace`
+   ```bash
+   nvidia-docker run --rm -t -v /sheet_music/:/workspace saty/muscima
+   ```
+4. Inference will now be performed on all image files in the `/sheet_music` directory. The results will be returned to `/sheet_music/output`
 
-and put it into the demo directory.
+## Using optional arguments
 
-Then run `standalone_inference_over_image.py` from inside the demo directory:
+The following arguments are available:
+```
+usage: standalone_inference_over_image.py [-h]
+                    [--detection_inference_graph DETECTION_INFERENCE_GRAPH]
+                    [--input_directory INPUT_DIRECTORY]
+                    [--detection_label_map DETECTION_LABEL_MAP]
+                    [--output_dir OUTPUT_DIR]
 
-```bash
-python standalone_inference_over_image.py \
-    --detection_inference_graph 2018-07-30_faster-rcnn_inception-resnet-v2_full-page_muscima.pb \
-    --input_image w-21_p008.png \
-    --detection_label_map category_mapping.txt \
-    --output_image annotated_image.jpg \
-    --output_result output_transcript.txt
+Performs detection over input image given a trained detector.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --detection_inference_graph DETECTION_INFERENCE_GRAPH
+                        Path to the frozen inference graph.
+  --input_directory INPUT_DIRECTORY
+                        Path to the input image.
+  --detection_label_map DETECTION_LABEL_MAP
+                        Path to the label map, which maps each categoryname to
+                        a unique number. Must be a simple text-file with one
+                        mapping per line in the form of:"<number> <label>",
+                        e.g. "1 barline".
+  --output_dir OUTPUT_DIR
+                        where to output results to.
 ```
 
-Note that the depicted parameters are the default-parameter, so if you are happy with them, they can be omitted (e.g. the label_maps).
+To use these arguments, use the following docker commands:
+```bash
+docker run -rm -v /sheet_music/:/workspace --entrypoint /code/standalone_inference_over_image.py saty/muscima [OPTIONS]
+```
+Here, we assume that you want to still perform inference on files in the `/sheet_music` directory. The `/sheet_music` directory is then mapped to `/workspace/` in the docker container.
+For further reference on running optional arguments, refer to the [docker reference](https://docs.docker.com/engine/reference/run/#entrypoint-default-command-to-execute-at-runtime) for `docker run` 
--- MusicObjectDetector/demo/standalone_inference_over_image.py
+++ MusicObjectDetector/demo/standalone_inference_over_image.py
@@ -1,12 +1,18 @@
 import argparse
 import os
+import os.path as osp
 
+# Ignore future warnings from numpy
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import cv2
 import numpy as np
 import tensorflow as tf
+from tqdm import tqdm
 from PIL import Image, ImageColor
 
-STANDARD_COLORS = [
+
+COLORS = [
     'AliceBlue', 'Chartreuse', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque',
     'BlanchedAlmond', 'BlueViolet', 'BurlyWood', 'CadetBlue', 'AntiqueWhite',
     'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan',
@@ -86,72 +92,169 @@ def build_map(path_to_labelmap):
     return int2category
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Performs detection over input image given a trained detector.')
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Performs detection over input image given a trained '
+                    'detector.'
+    )
     parser.add_argument('--detection_inference_graph', type=str,
-                        default="2018-05-15_faster-rcnn_inception-resnet-v2_2000-proposals_full-page-detection_muscima-pp.pb",
                         help='Path to the frozen inference graph.')
-    parser.add_argument('--input_image', type=str, default="w-21_p008.png", help='Path to the input image.')
-    parser.add_argument('--detection_label_map', type=str, default="category_mapping.txt",
-                        help='Path to the label map, which maps each category name to a unique number.'
-                             'Must be a simple text-file with one mapping per line in the form of:'
-                             '"<number> <label>", e.g. "1 barline".')
-    parser.add_argument('--output_image', type=str, default="annotated_image.jpg",
-                        help='Path to the output image, with highlighted boxes.')
-    parser.add_argument('--output_result', type=str, default="output_transcript.txt",
-                        help='Path to the output file, that will contain a list of detection, '
-                             'including position-classification')
-    args = parser.parse_args()
+    parser.add_argument('--input_directory', type=str,
+                        help='Path to the input image.')
+    parser.add_argument('--detection_label_map', type=str,
+                        default="category_mapping.txt",
+                        help='Path to the label map, which maps each category'
+                             'name to a unique number. Must be a simple '
+                             'text-file with one mapping per line in the form '
+                             'of:"<number> <label>", e.g. "1 barline".')
+    parser.add_argument('--output_dir', type=str,
+                        help='where to output results to.')
+    return parser.parse_args()
 
-    # The next line forces Tensorflow on Windows to run the computation on the CPU
-    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+if __name__ == "__main__":
+    args = parse_args()
+    # The next line forces Tensorflow on Windows to run the computation on CPU
+    # os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+    # Make sure the output dir exists
+    os.makedirs(args.output_dir)
 
     # Build category map
-    detection_category_mapping = build_map(args.detection_label_map)
-    class_to_index_mapping = {value: key for key, value in detection_category_mapping.items()}
+    detection_cat_mapping = build_map(args.detection_label_map)
+    class_idx_mapping = {value: key
+                         for key, value in detection_cat_mapping.items()}
 
     # Read frozen graphs
     detection_graph = load_detection_graph(args.detection_inference_graph)
 
-    # PIL Image
-    image = Image.open(args.input_image).convert("RGB")
-    (image_width, image_height) = image.size
+    # Get all files in the input directory
+    images = os.listdir(args.input_directory)
+    valid_extensions = ('.jpg', '.png', '.jpeg', '.tiff', '.bmp', '.gif')
+    valid_images = []
+    for img in images:
+        if img.lower().endswith(valid_extensions):
+            valid_images.append(img)
 
-    # Opencv Image (draw)
-    image_cv = cv2.imread(args.input_image, True)
+    # start the session.
+    with detection_graph.as_default():
+        with tf.Session() as sess:
+            default_graph = tf.get_default_graph()
+            ops = default_graph.get_operations()
+            all_tensor_names = {output.name
+                                for op in ops
+                                for output in op.outputs}
+            tensor_dict = {}
+            keys_to_check = ('num_detections', 'detection_boxes',
+                             'detection_scores', 'detection_classes')
+            for key in keys_to_check:
+                tensor_name = key + ':0'
+                if tensor_name in all_tensor_names:
+                    tensor_dict[
+                        key] = tf.get_default_graph().get_tensor_by_name(
+                        tensor_name)
 
-    # Numpy image
-    image_np = np.array(image.getdata()).reshape((image_height, image_width, 3)).astype(np.uint8)
+            # Now iterate through images
+            print("running detection")
+            image_arrays = []
+            raw_images = []
+            for img_name in tqdm(valid_images):
+                # Open each file as a PIL
+                img_fp = osp.join(args.input_directory, img_name)
 
-    # Actual detection;
-    output_dict = run_inference_for_single_image(image_np, detection_graph)
-    output_lines = []
+                # Opencv Image (draw)
+                image_cv = cv2.imread(img_fp)
+                # Resize everything to the same size. This size is A4 at 144 DPI
+                image_cv = cv2.resize(image_cv, (1190, 1684))
+                height, width, _ = image_cv.shape
+                raw_images.append({
+                    'image_name': img_name,
+                    'image': image_cv,
+                    'height': height,
+                    'width': width
+                })
 
-    for idx in range(output_dict['num_detections']):
-        if output_dict['detection_scores'][idx] > 0.5:
+                # Numpy image
+                image_arrays.append(np.array(image_cv))
 
-            y1, x1, y2, x2 = output_dict['detection_boxes'][idx]
+                if len(image_arrays) < 1:
+                    continue
 
-            y1 = y1 * image_height
-            y2 = y2 * image_height
-            x1 = x1 * image_width
-            x2 = x2 * image_width
-            detected_class = detection_category_mapping[output_dict['detection_classes'][idx]]
+                # Otherwise prepare for detection
+                if len(image_arrays) > 1:
+                    stacked_array = np.stack(image_arrays)
+                else:
+                    stacked_array = np.expand_dims(image_arrays[0], 0)
+                image_arrays = []  # Reset the image_arrays list
 
-            output_line = "{0:.3f},{1:.3f},{2:.3f},{3:.3f};{4}".format(x1, y1, x2, y2, detected_class)
-            print(output_line)
-            output_lines.append(output_line)
+                # Actual detection
+                image_tensor = default_graph.get_tensor_by_name(
+                    'image_tensor:0'
+                )
 
-            if args.output_image is not None:
-                color_name = STANDARD_COLORS[class_to_index_mapping[detected_class] % len(STANDARD_COLORS)]
-                color_rgb = ImageColor.getrgb(color_name)
-                cv2.rectangle(image_cv, (int(x1), int(y1)), (int(x2), int(y2)), color_rgb, 3)
+                session_result = sess.run(
+                    tensor_dict,
+                    feed_dict={image_tensor: stacked_array}
+                )
 
-        else:
-            break
+                # Post process detections
+                processed_dictionaries = []
+                for i in range(len(session_result['num_detections'])):
+                    processed_dictionaries.append({
+                        'num_detections':
+                            int(session_result['num_detections'][i]),
+                        'detection_classes':
+                            session_result['detection_classes'][i]
+                                .astype(np.uint8),
+                        'detection_boxes':
+                            session_result['detection_boxes'][i],
+                        'detection_scores':
+                            session_result['detection_scores'][i]
+                    })
 
-    if args.output_image is not None:
-        cv2.imwrite(args.output_image, image_cv)
+                output_lines = ['x1,y1,x2,y2,det_class']
 
-    with open(args.output_result, "w") as output_file:
-        output_file.write("\n".join(output_lines))
+                for raw_image, out_dict in zip(raw_images,
+                                               processed_dictionaries):
+                    image_cv = raw_image['image']
+                    img_name = raw_image['image_name']
+                    image_height = raw_image['height']
+                    image_width = raw_image['width']
+                    for idx in range(out_dict['num_detections']):
+                        if out_dict['detection_scores'][idx] > 0.5:
+                            # Only draw rectangles with a score above 0.5
+                            y1, x1, y2, x2 = out_dict['detection_boxes'][idx]
+
+                            y1 = y1 * image_height
+                            y2 = y2 * image_height
+                            x1 = x1 * image_width
+                            x2 = x2 * image_width
+                            detected_class = detection_cat_mapping[
+                                out_dict['detection_classes'][idx]
+                            ]
+
+                            output_line = "{:.3f},{:.3f},{:.3f},{:.3f};{}"\
+                                .format(x1, y1, x2, y2, detected_class)
+                            output_lines.append(output_line)
+
+                            # Draw the bbox onto the image
+                            color_idx = class_idx_mapping[detected_class] % len(COLORS)
+                            color_name = COLORS[color_idx]
+                            color_rgb = ImageColor.getrgb(color_name)
+                            cv2.rectangle(image_cv, (int(x1), int(y1)),
+                                          (int(x2), int(y2)), color_rgb, 3)
+
+                        else:
+                            # Once we get to a score below 0.5, the rest will presumably
+                            # also be below 0.5 so we stop
+                            break
+
+                    cv2.imwrite(osp.join(args.output_dir, img_name), image_cv)
+
+                    text_file_fp = osp.join(args.output_dir,
+                                            osp.splitext(img_name)[0] + '.csv')
+
+                    with open(text_file_fp, "w") as output_file:
+                        output_file.write("\n".join(output_lines))
+
+                raw_images = []
